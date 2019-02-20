36c22b79ae351783180ca54bd9d852778dc43472
Esthertc <teranesther97@outlook.es>
2019-02-20 03:02:16 +0100

    empezandoconhtmlyjsbasico

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-/* Acá va tu código */
+const inputMessage=document.getElementById("message");
+const parrafResult=document.getElementById("result");
+
+const btncode=document.getElementById("btncode");
+btncode.addEventListener("click",() =>{
+inputMessage.value;
+console.log ("click cifrar", inputMessage.value.toUpperCase());
+result.innerHTML="cifrado "+ inputMessage.value.toUpperCase();
+})
+const btndecipher=document.getElementById("btndecipher");
+btndecipher.addEventListener("click",() =>{
+inputMessage.value.toUpperCase;
+console.log("click descifrar",inputMessage.value.toUpperCase());
+result.innerHTML="descifrado "+ inputMessage.value.toUpperCase();
+})
