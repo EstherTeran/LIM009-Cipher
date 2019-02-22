db2bace525998eed63390c45f22e67dc7f02e07f
Esthertc <teranesther97@outlook.es>
2019-02-22 13:14:42 -0500

    un poco mas css

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,18 @@
 window.cipher = {
   encode: (offset,texto) => {
-  const longitud=texto.length;
+  /*const longitud=texto.length;
 for(let i = 1;i<longitud;i++){
   console.log(texto.charCodeAt(i))}
-  console.log(texto.charCodeAt());
-  console.log(window)
+  console.log(texto.charCodeAt());*/
+  const palabra = "A"
+var offset=3;
+var asciinumero= frases.charCodeAt();
+var nuevoascii = (asciinumero-65+8)%26+65;
+var nuevaletra= String.fromCharCode(nuevoascii);
+console.log(nuevaletra)
+
 },
-  decode: (offset, string) => {
+ decode: (offset, string) => {
     /* Acá va tu código */
   }
 };
