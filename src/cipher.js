3b14f3bee4129dd92b0eae7ecd0645d3667faa20
Esthertc <teranesther97@outlook.es>
2019-02-22 06:37:11 -0500

    empezando css

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,10 @@
 window.cipher = {
   encode: (offset,texto) => {
-const longitud=texto.length;
+  const longitud=texto.length;
 for(let i = 1;i<longitud;i++){
   console.log(texto.charCodeAt(i))}
-console.log(texto.charCodeAt())
+  console.log(texto.charCodeAt());
+  console.log(window)
 },
   decode: (offset, string) => {
     /* Acá va tu código */
