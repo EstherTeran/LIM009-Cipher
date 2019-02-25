9042eb1b79db020f7c63f5d1b02e632a616ce8c3
Esthertc <teranesther97@outlook.es>
2019-02-24 22:59:14 -0500

    cifra una palabra y la muestra en html

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,21 @@
 window.cipher = {
-  encode: (offset,texto) => {
-  /*const longitud=texto.length;
-for(let i = 1;i<longitud;i++){
-  console.log(texto.charCodeAt(i))}
-  console.log(texto.charCodeAt());*/
-  const palabra = "A"
-var offset=3;
-var asciinumero= frases.charCodeAt();
-var nuevoascii = (asciinumero-65+8)%26+65;
-var nuevaletra= String.fromCharCode(nuevoascii);
-console.log(nuevaletra)
-
-},
- decode: (offset, string) => {
+  encode: (offset, texto) => {
+    const lengthText = texto.length; //2 AB
+    let textencode=""
+    for (let i = 0; i < lengthText; i++) {
+      const positionAscii = texto.charCodeAt(i);//65--posiciòn  en codigo ascii
+      //console.log(typeof offset, offset, positionAscii - 65 , positionAscii - 65 + offset )
+      const increasedPosition = ((positionAscii - 65 + offset) % 26) + 65;//68-- cifrado (le suma el desplazamiento a la pocisiòn)
+      //console.log(increasedPosition)
+      const encodeLetter = String.fromCharCode(increasedPosition);
+      //console.log(encodeLetter);
+      textencode=textencode+encodeLetter
+    }
+      return textencode
+  }
+  ,
+  decode: (offset, string) => {
     /* Acá va tu código */
   }
 };
+// console.log(newAlphabet)
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-const inputMessage=document.getElementById("message");
+const inputtext=document.getElementById("inputtext");
 const paragraphResult=document.getElementById("result");
-const inputNumber=document.getElementById("optionsnumber")
+const inputoffset=document.getElementById("inputoffset")
 
-const btncode=document.getElementById("btncode");
-btncode.addEventListener("click",() =>{
-inputMessage.value;
-console.log ("click cifrar", inputMessage.value.toUpperCase());
-result.innerHTML="cifrado "+ inputMessage.value.toUpperCase();
-})
+const btnencode=document.getElementById("btnencode");
+btnencode.addEventListener("click", () => {
+    window.cipher.encode(parseInt(inputoffset.value),inputtext.value.toUpperCase());
+    result.innerHTML=window.cipher.encode(parseInt(inputoffset.value),inputtext.value.toUpperCase());
+});
+// btnencode.addEventListener("click",() =>{
+// inputMessage.value;
+// console.log ("click cifrar", inputMessage.value.toUpperCase());
+// result.innerHTML="cifrado "+ inputMessage.value.toUpperCase();
+// })
 const btndecipher=document.getElementById("btndecipher");
-btndecipher.addEventListener("click",() =>{
-inputMessage.value.toUpperCase;
-console.log("click descifrar",inputMessage.value.toUpperCase());
-result.innerHTML="descifrado "+ inputMessage.value.toUpperCase();
-})
+// btndecipher.addEventListener("click",() =>{
+// inputMessage.value.toUpperCase;
+// console.log("click descifrar",inputMessage.value.toUpperCase());
+// result.innerHTML="descifrado "+ inputMessage.value.toUpperCase();
+// })
+
+
