9d831cfd0692ff19fb5662fcfd4b71824b05c968
Esthertc <teranesther97@outlook.es>
2019-02-27 12:51:02 -0500

    test

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,8 @@
 window.cipher = {
   encode: (offset, texto) => {
-    const lengthText = texto.length; //2 AB
-    let textencode = ""
-    for (let i = 0; i < lengthText; i++) {
+   // const lengthText = texto.length; //2 AB
+    let textencode = "";
+    for (let i = 0; i < texto.length; i++) {
       const positionAscii = texto.charCodeAt(i);//65--posiciòn  en codigo ascii
       const increasedPosition = ((positionAscii - 65 + offset) % 26) + 65;//68-- cifrado (le suma el desplazamiento a la posisiòn)
       const encodeLetter = String.fromCharCode(increasedPosition);
@@ -12,26 +12,14 @@ window.cipher = {
   }
   ,
   decode: (offset, texto) => {
-    const lengthText = texto.length; //2 AB
-    let textencode = ""
-    for (let i = 0; i < lengthText; i++) {
+    //const lengthText = texto.length;
+    let textencode = "";
+    for (let i = 0; i < texto.length; i++) {
       const positionAscii = texto.charCodeAt(i);//65--posiciòn  en codigo ascii
-      const increasedPosition = ((positionAscii -90 - offset) % 26) + 90;//68-- cifrado (le suma el desplazamiento a la posisiòn)
+      const increasedPosition = ((positionAscii - 90 - offset) % 26) + 90;//68-- cifrado (le suma el desplazamiento a la posisiòn)
       const encodeLetter = String.fromCharCode(increasedPosition);
       textencode = textencode + encodeLetter
     }
     return textencode
-
-   
-    /*const lengthText = texto ; //2 AB
-    let textdecode = ""
-    for (let i = 0; i < lengthText; i++) {
-      const positionAscii = texto.charCodeAt(i);//65--posiciòn  en codigo ascii
-      const increasedPosition = ((positionAscii - 65 + offset) % 26) + 65;//68-- cifrado (le suma el desplazamiento a la posisiòn)
-      const decodeLetter = String.fromCharCode(increasedPosition);
-      textdecode = textdecode + decodeLetter
-    }
-    return textdecode*/
-
   }
 };
--- src/index.js
+++ src/index.js
@@ -4,18 +4,11 @@ const inputoffset=document.getElementById("inputoffset")
 
 const btnencode=document.getElementById("btnencode");
 btnencode.addEventListener("click", () => {
-    // window.cipher.encode(parseInt(inputoffset.value),inputtext.value.toUpperCase());
-    result.innerHTML=window.cipher.encode(parseInt(inputoffset.value),inputtext.value.toUpperCase());
+ paragraphResult.innerHTML=window.cipher.encode(parseInt(inputoffset.value),inputtext.value.toUpperCase());
 });
 const btndecipher=document.getElementById("btndecipher");
 btndecipher.addEventListener("click", () =>{
-    // window.cipher.decode(inputoffset.value),inputtext.value.toUpperCase();
-    result.innerHTML=window.cipher.decode(parseInt(inputoffset.value),inputtext.value.toUpperCase());
+    paragraphResult.innerHTML=window.cipher.decode(parseInt(inputoffset.value),inputtext.value.toUpperCase());
 })
-// btndecipher.addEventListener("click",() =>{
-// inputMessage.value.toUpperCase;
-// console.log("click descifrar",inputMessage.value.toUpperCase());
-// result.innerHTML="descifrado "+ inputMessage.value.toUpperCase();
-// })
 
 
